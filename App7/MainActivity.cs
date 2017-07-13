635ec677b49124a9d1660b15ac9463880bcc648e
Tuo04 <chentuo01@beyondsoft.com>
2017-07-12 23:17:28 -0700

    Modify Code

--- App7/MainActivity.cs
+++ App7/MainActivity.cs
@@ -1,6 +1,10 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
+using Microsoft.Azure.Mobile.Push;
 
 namespace App7
 {
@@ -12,7 +16,41 @@ namespace App7
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+            SetContentView (Resource.Layout.Main);
+
+            // This should come before MobileCenter.Start() is called
+            Push.PushNotificationReceived += (sender, e) => {
+
+                // Add the notification message and title to the message
+                var summary = $"Push notification received:" +
+                                    $"\n\tNotification title: {e.Title}" +
+                                    $"\n\tMessage: {e.Message}";
+
+                // If there is custom data associated with the notification,
+                // print the entries
+                if (e.CustomData != null)
+                {
+                    summary += "\n\tCustom data:\n";
+                    foreach (var key in e.CustomData.Keys)
+                    {
+                        summary += $"\t\t{key} : {e.CustomData[key]}\n";
+                    }
+                }
+
+                // Send the notification summary to debug output
+                System.Diagnostics.Debug.WriteLine(summary);
+            };
+
+            Button button = FindViewById<Button>(Resource.Id.button1);
+            button.Click += delegate { onCrash(); };
+
+            MobileCenter.Start("861d550c-b8f5-4c90-a39e-e367a110ec7b",
+                   typeof(Analytics), typeof(Crashes),typeof(Push));
+
+        }
+
+        public void onCrash() {
+            Crashes.GenerateTestCrash();
         }
     }
 }
